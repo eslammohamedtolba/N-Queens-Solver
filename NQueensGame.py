392a8203fa38676a6cf2d813fafc107c046e8897
Eslam <eslam3012mohamed@gmail.com>
2023-10-06 22:47:29 +0300

    add N Queens game file that allows you to create a chessboard and solve it with yourself or by the solver

---
+++ NQueensGame.py
@@ -0,0 +1,16 @@
+'''
+-The N Queen Problem is a classic problem of placing N chess queens on an NxN chessboard so that no two queens attack each other.
+-Two queens attack each other if are in the same row or column or diagonal from top-right to left-bottom or diagonal from top-left to   right-bottom
+-This program allows you to create a chessboard and solve it with yourself or by the solver
+
+'''
+
+# Importing the GameRunning class
+from GameRunning import *
+
+
+# Creating an instance of the GameRunning class and Starting the game
+gamerun = GameRunning()
+gamerun.StartGame()
+
+
